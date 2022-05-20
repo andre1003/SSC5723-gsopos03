2e0a0d0c21578a545d50dc70e1a1c7f9ecf62888
Paulo André Pimenta Aragão <andrearagao1003@outlook.com>
2022-05-20 11:39:49 -0300

    Insert and consume functions added to Module 2

--- Module_2/main.c
+++ Module_2/main.c
@@ -12,7 +12,7 @@ Tem que ter:
 /* INCLUDES */
 #include "stdlib.h"
 #include "stdio.h"
-#include "pthreads.h"
+//#include "pthreads.h"
 
 
 
@@ -25,16 +25,81 @@ typedef struct BUFFER {
 
 
 /* FUNCTIONS */
-buffer* insert(buffer* bffr, int id) {
-    return NULL;
+buffer* insert(buffer* head, int id) {
+    buffer* node = (buffer*) malloc (sizeof(buffer));
+    node->id = id;
+    node->next = NULL; // For now, this is a queue
+
+    if(!head)
+        return node;
+
+    buffer* aux = head;
+    while(aux->next)
+        aux = aux->next;
+
+    aux->next = node;
+
+    return head;
 }
 
-buffer* remove(buffer* bffr, int id) {
-    return NULL;
+buffer* consume(buffer* head, int id) {
+    buffer* current = head;
+    buffer* previous = NULL;
+
+    while(current && current->id != id) {
+        previous = current;
+        current = current->next;
+    }
+
+    if(current) {
+        if(previous) {
+            previous->next = current->next;
+            free(current);
+        }
+        else {
+            previous = current->next;
+            free(current);
+            return previous;
+        }
+    }
+    else {
+        printf("This value was not found on the list...\n");
+    }
+
+    return head;
+}
+
+// Print
+void printBuffer(buffer* head) {
+    buffer* aux = head;
+
+    while(aux != NULL) {
+        printf("%d", aux->id);
+
+        if(aux->next != NULL)
+            printf(" -> ");
+        else
+            printf("\n");
+
+        aux = aux->next;
+    }
 }
 
 
 /* MAIN */
 int main() {
-    return 0;
+    buffer* head = NULL;
+    int i, consumeId;
+
+    for(i = 0; i < 5; i++)
+        head = insert(head, i);
+
+    printBuffer(head);
+
+    for(i = 0; i < 3; i++) {
+        printf("\nType a value to be removed: ");
+        scanf("%d", &consumeId);
+        head = consume(head, consumeId);
+        printBuffer(head);
+    }
 }
