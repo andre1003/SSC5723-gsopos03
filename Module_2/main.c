d3e37a6bd34c6baebf201203dd0da5e54206bbac
Paulo André Pimenta Aragão <andrearagao1003@outlook.com>
2022-05-23 14:21:01 -0300

    Module 2 Update

--- Module_2/main.c
+++ Module_2/main.c
@@ -1,105 +1,227 @@
-/*
-- Produtor
-- Consumidor
-- Controle de buffer (struct)
-    -> Cheio
-    -> Vazio
+/* DEFINES */
+#define MAX_BUFFER_SIZE 10
 
-Tem que ter:
-- Semáforo
-*/
+
 
 /* INCLUDES */
 #include "stdlib.h"
 #include "stdio.h"
-//#include "pthreads.h"
+#include "pthread.h"
 
 
 
 
 /* STRUCTS */
+// Node struct
+typedef struct NODE {
+    int id;             // Node ID
+    int isEmpty;        // 1 - is empty | 0 - is full
+    struct NODE* next;  // Next element
+} node;
+
+
+// Buffer struct
 typedef struct BUFFER {
-    int id;
-    struct BUFFER* next;
+    pthread_cond_t full;    // Full semaphore
+    pthread_cond_t empty;   // Empty semaphore
+    pthread_mutex_t mutex;  // Mutex semaphore
+
+    int countFull;          // Full slots counter
+    int countEmpty;         // Empty slots counter
+
+    node* head;             // Node list head
+    node* nextEmpty;        // Next empty memory slot
+    node* nextFull;         // Next full memory slot
 } buffer;
 
 
+
+
 /* FUNCTIONS */
-buffer* insert(buffer* head, int id) {
-    buffer* node = (buffer*) malloc (sizeof(buffer));
-    node->id = id;
-    node->next = NULL; // For now, this is a queue
+/// <summary>
+/// Creates the buffer.
+/// </summary>
+/// <param name="buff">Buffer struct.</param>
+/// <returns>Created buffer.</returns>
+buffer create(buffer buff) {
+    buff.countFull = 0;
+    buff.countEmpty = MAX_BUFFER_SIZE;
 
-    if(!head)
-        return node;
+    buff.head = NULL;
+    buff.nextFull = NULL;
+    buff.nextEmpty = NULL;
 
-    buffer* aux = head;
-    while(aux->next)
-        aux = aux->next;
-
-    aux->next = node;
-
-    return head;
+    return buff;
 }
 
-buffer* consume(buffer* head, int id) {
-    buffer* current = head;
-    buffer* previous = NULL;
+/// <summary>
+/// Initializes buffer. Creates the node list and sets the semaphores.
+/// </summary>
+/// <param name="buff">Buffer struct.</param>
+/// <returns>Initializes buffer.</returns>
+buffer init(buffer buff) {
+    // Create node list
+    int i;
 
-    while(current && current->id != id) {
-        previous = current;
-        current = current->next;
-    }
+    // Insert MAX_BUFFER_SIZE nodes in the buffer head list
+    for(i = 0; i < MAX_BUFFER_SIZE; i++)
+        buff.head = add(buff.head, i, 1);
 
-    if(current) {
-        if(previous) {
-            previous->next = current->next;
-            free(current);
-        }
-        else {
-            previous = current->next;
-            free(current);
-            return previous;
-        }
-    }
-    else {
-        printf("This value was not found on the list...\n");
-    }
+    buff.nextEmpty = buff.head;             // The next empty element is the first buffer list element
 
-    return head;
+    // Semaphores init with NULL value
+    pthread_mutex_init(&buff.mutex, NULL);  // Mutex
+    pthread_cond_init(&buff.empty, NULL);   // Empty
+    pthread_cond_init(&buff.full, NULL);    // Full
+
+    // Return buffer
+    return buff;
 }
 
-// Print
-void printBuffer(buffer* head) {
-    buffer* aux = head;
 
+/// <summary>
+/// Insert a node into a given list
+/// </summary>
+/// <param name="head">List to insert the node.</param>
+/// <param name="id">Node's ID.</param>
+/// <param name="isEmpty">Is the node empty.</param>
+/// <returns>Return the full list.</returns>
+node* add(node* head, int id, int isEmpty) {
+    // Allocates the node
+    node* node = (node*) malloc (sizeof(node));
+
+    // Node variables setting
+    node->id = id;                  // ID
+    node->isEmpty = isEmpty;        // Is empty
+    node->next = NULL;              // Next element
+
+    // Insert the element
+    if(!head)                       // If head is null
+        return node;                // Return the node
+
+    node* current = head;           // Init the current node at the list's head
+    while(current->next)            // While the current node has a next element
+        current = current->next;    // Go through the list
+
+    current->next = node;           // Insert the created node
+
+    return head;                    // Return the list
+}
+
+
+/// <summary>
+/// Remove a node by id.
+/// </summary>
+/// <param name="head">The first node list element.</param>
+/// <param name="id">The node's ID to be removed.</param>
+/// <returns>The node list without the removed node.</returns>
+node* del(node* head, int id) {
+	node* current = head;
+	node* previous = NULL;
+
+    // Try to find the node to be removed
+	while(current && current->id != id) {
+		previous = current;
+		current = current->next;
+	}
+
+    // If there is a node to be removed
+	if (current) {
+        // Is not the first element
+		if(previous) {
+			previous->next = current->next;
+			free(current);
+		}
+        // Is the first element
+		else {
+			previous = current->next;
+			free(current);
+			return previous;
+		}
+	}
+    // Node not founded
+	else {
+		printf("This value was not found on the list...\n");
+	}
+
+    // Return the node list
+	return head;
+}
+
+
+/// <summary>
+/// Print the node list.
+/// </summary>
+/// <param name="head">Node list first element.</param>
+void printData(node* head) {
+    node* aux = head;
+
+    // Go through the node list
     while(aux != NULL) {
-        printf("%d", aux->id);
+        printf("%d", aux->id);  // Print the id
 
         if(aux->next != NULL)
             printf(" -> ");
         else
             printf("\n");
 
-        aux = aux->next;
+        aux = aux->next;               // Get the next element
     }
 }
 
 
+/// <summary>
+/// Destroy the buffer struct.
+/// </summary>
+/// <param name="buff"> Buffer struct.</param>
+/// <returns>Return the destroyed buffer.</returns>
+buffer destroy(buffer buff) {
+    // Destroy the semaphores
+    pthread_cond_destroy(&buff.empty);  // Empty
+    pthread_cond_destroy(&buff.full);   // Full
+    pthread_mutex_destroy(&buff.mutex); // Mutex
+
+    // Destroy the node list
+    for (i = 0; i < MAX_BUFFER_SIZE; i++)
+        buff.head = del(buff.head, i);
+
+    // Return an empty buffer struct
+    return NULL;
+}
+
+
+void* produce() {
+
+}
+
+
+void* consume() {
+
+}
+
+
+
+
 /* MAIN */
 int main() {
-    buffer* head = NULL;
-    int i, consumeId;
+    // Create buffer
+    buffer buff;                                    // Buffer
+    buff = create(buff);                            // Create buffer
+    buff = init(buff);                              // Init buffer
 
-    for(i = 0; i < 5; i++)
-        head = insert(head, i);
+    // Create the threads
+    pthread_t producer;                             // Producer thread variable
+    pthread_t consumer;                             // Consumer thread variable
 
-    printBuffer(head);
+    pthread_create(&producer, NULL, produce, NULL); // Producer thread creation
+    pthread_create(&consumer, NULL, consume, NULL); // Consumer thread creation
 
-    for(i = 0; i < 3; i++) {
-        printf("\nType a value to be removed: ");
-        scanf("%d", &consumeId);
-        head = consume(head, consumeId);
-        printBuffer(head);
-    }
+    pthread_join(producer, NULL);                   // Wait for the producer end
+    pthread_join(consumer, NULL);                   // Wait for the consumer end
+
+    // Destroy the buffer
+    buff = destroy(buff);                           // Destroy the buffer struct completely
+
+    // Exit
+    return 0;
 }
